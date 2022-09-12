9b9874e29ffaf69cd6de06510aae4c7532690559
mrinnnmoy <mrinmoy202000@gmail.com>
2022-09-12 21:31:26 +0530

    added script to value accordion

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,22 @@ var swiperPopular = new Swiper(".popular__container", {
 });
 
 /*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item')
 
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value__accordion-header')
+
+    accordionHeader.addEventListener('click', ()=>{
+        toggleItem(item)
+    })
+})
+
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+    accordionContent.style.height = accordionContent.scrollHeight + 'px'
+    item.classList.add('accordion-open')
+}
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
